c0f7f2436d31cf72f29a3661c466b5dcdd284559
wesleybertipaglia <wesleybertipaglia@gmail.com>
2025-01-18 12:55:09 -0300

    feat: update app.js
    Add func adicionarAmigo

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+const amigos = [];
+
+const eAmigoInput = document.querySelector("#amigo");
+const eAmigoList = document.querySelector("#listaAmigos");
+const eAmigoResultado = document.querySelector("#resultado");
+
+function adicionarAmigo(event) {
+  event.preventDefault();
+
+  const amigo = eAmigoInput.value.trim();
+
+  amigos.push(amigo);
+  eAmigoInput.value = "";
+}
